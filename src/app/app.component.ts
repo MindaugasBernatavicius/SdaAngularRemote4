1f7c5b3e41081a130b230245b74325c9a1f5d80c
Mindaugas <darbas.mindaugas@gmail.com>
2021-02-21 15:17:06 +0200

    Adding angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <app-header></app-header>
+    <h1>{{ greetingMessage }}!!</h1>
+    <h3 id="app_title">{{ title }}</h3>
+    <app-footer></app-footer>
+  `,
+  styles: [
+    '#app_title { color: red; }'
+  ]
 })
 export class AppComponent {
   title = 'SdaAngularRemote4';
+  height = 5;
+  greetingMessage = 'Hello word!';
 }
